43d12fb0d1060dae4d1535905e71528c7c219c1b
IvanTuly <ivantuly@gmail.com>
2023-01-14 16:03:43 +0300

    before deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
         <header className="d-flex justify-between align-center">
             <Link to="/">
             <div className="headerLeft d-flex align-center"> 
-                <img width={40} height={40} src="/img/logo.png" alt="logo"/>
+                <img width={40} height={40} src="./img/logo.png" alt="logo"/>
                 <div className="headerInfo">
                     <h3 className="text-uppercase">React Sneakers</h3>
                     <p className="opacity-5">Best sneakers store</p>
